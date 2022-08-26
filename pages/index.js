d6210f36f85f79a1b57420b5eb6396162d461f4f
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-08-25 22:22:24 -0500

    staticProp options

--- pages/index.js
+++ pages/index.js
@@ -12,15 +12,28 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/do-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 
